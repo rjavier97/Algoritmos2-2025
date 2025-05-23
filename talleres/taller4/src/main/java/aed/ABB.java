25f5bda344171a7a3d98eb4a3218473f0463d02f
rjavier97 <rjavier.suarez@hotmail.com>
2025-05-23 18:33:37 -0300

    terminado el metodo eliminar

--- talleres/taller4/src/main/java/aed/ABB.java
+++ talleres/taller4/src/main/java/aed/ABB.java
@@ -109,56 +109,102 @@ public class ABB<T extends Comparable<T>> implements Conjunto<T> {
         return raiz != null && elem.compareTo(ultimo_nodo_buscado.valor)==0 ;
     }       
 
-    public void eliminar(T elem){
-        Nodo ultimo_nodo_buscado = buscar_nodo(elem, raiz) ;
-        if (ultimo_nodo_buscado.valor.compareTo(elem) == 0){ //si el nodo pertenece al ABB entonces se puede eliminar
-            if (ultimo_nodo_buscado == raiz){ // El ABB tiene solo la raiz, y la raiz es el nodo a eliminar
-                raiz = null;
-                cardinal --;
-            }
-            if (ultimo_nodo_buscado.padre != null && ultimo_nodo_buscado.izq==null && ultimo_nodo_buscado.der==null){ // No tiene hijos 
-                if (ultimo_nodo_buscado.valor.compareTo(ultimo_nodo_buscado.padre.valor)<0){
-                    ultimo_nodo_buscado.padre.izq = null; 
-                    ultimo_nodo_buscado = null;
-                    cardinal --;
-                } else {
-                    ultimo_nodo_buscado.padre.der = null; 
-                    ultimo_nodo_buscado = null;
-                    cardinal --;
-                }
-            }
-            if (ultimo_nodo_buscado.der == null || ultimo_nodo_buscado.izq == null){  //Tiene solo un hijo
-                if(ultimo_nodo_buscado.der != null){  // el hijo del elemento, esta del lado derecho
-                    if (ultimo_nodo_buscado.valor.compareTo(ultimo_nodo_buscado.padre.valor)<0){
-                        ultimo_nodo_buscado = ultimo_nodo_buscado.der ;
-                        ultimo_nodo_buscado.padre.izq = ultimo_nodo_buscado;         
-                        cardinal --;  // el elemento a eliminar es menor que el padre
-                    } else {
-                        ultimo_nodo_buscado.padre.der = ultimo_nodo_buscado.der ;
-                        ultimo_nodo_buscado.der.padre = ultimo_nodo_buscado.padre;
-                        cardinal --;  // el elemento a eliminar es mayor que el padre                
-                    }
-                } else {  // el hijo del elemento, esta del lado izquierdo
-                    if (ultimo_nodo_buscado.valor.compareTo(ultimo_nodo_buscado.padre.valor)<0){ 
-                        ultimo_nodo_buscado = ultimo_nodo_buscado.izq ;
-                        ultimo_nodo_buscado.padre.izq = ultimo_nodo_buscado;         
-                        cardinal --;  // el elemento a eliminar es menor que el padre
-                    } else {
-                        ultimo_nodo_buscado.padre.der = ultimo_nodo_buscado.izq ;
-                        ultimo_nodo_buscado.izq.padre = ultimo_nodo_buscado.padre;
-                        cardinal --;   // el elemento a eliminar es mayor que el padre                  
-                    }  
+    // public void eliminar(T elem){
+    //     Nodo ultimo_nodo_buscado = buscar_nodo(elem, raiz) ;
+    //     if (ultimo_nodo_buscado.valor.compareTo(elem) == 0){ //si el nodo pertenece al ABB entonces se puede eliminar
+    //         if (ultimo_nodo_buscado == raiz){// El nodo a eliminar es la raiz
+    //             if (cardinal ==1){ // El nodo a eliminar es la raiz y el ABB tiene un solo elemento
+    //                 raiz = null;
+    //                 cardinal --;
+    //             } else { // El nodo a eliminar es la raiz y el ABB tiene mas de un elemento
+    //                 if (ultimo_nodo_buscado.der != null){ // El nodo a eliminar es la raiz y tiene sucesor
+    //                     Nodo sucesor = busqueda_minimo(ultimo_nodo_buscado.der);
+    //                     ultimo_nodo_buscado.valor = sucesor.valor;
+    //                     sucesor = null ;
+    //                 } else {  // El nodo a eliminar es la raiz y tiene antecesor
+    //                     Nodo antecesor = busqueda_maximo(ultimo_nodo_buscado.izq);
+    //                     ultimo_nodo_buscado.valor = antecesor.valor;
+    //                     antecesor = null ;
+    //                 }
+    //             }
+    //         } //----------------------------------------------------------------------------------------------------------------------------------------------------
+    //         if (ultimo_nodo_buscado.padre != null && ultimo_nodo_buscado.izq==null && ultimo_nodo_buscado.der==null){ // No tiene hijos 
+    //             if (ultimo_nodo_buscado.valor.compareTo(ultimo_nodo_buscado.padre.valor)<0){ // El hijo a eliminar es menor que el padre
+    //                 ultimo_nodo_buscado.padre.izq = null; 
+    //                 ultimo_nodo_buscado = null;
+    //                 cardinal --;
+    //             } else { // El hijo a eliminar es mayor que el padre 
+    //                 ultimo_nodo_buscado.padre.der = null; 
+    //                 ultimo_nodo_buscado = null;
+    //                 cardinal --;
+    //             }
+    //         }
+    //         if ( (ultimo_nodo_buscado.der == null && ultimo_nodo_buscado.izq != null) || (ultimo_nodo_buscado.izq == null && ultimo_nodo_buscado.der != null) ){  //Tiene solo un hijo
+    //             if(ultimo_nodo_buscado.der != null){  // el hijo del elemento a eliminar, esta del lado derecho
+    //                 if (ultimo_nodo_buscado.valor.compareTo(ultimo_nodo_buscado.padre.valor)<0){  // el elemento a eliminar es menor que el padre
+    //                     ultimo_nodo_buscado = ultimo_nodo_buscado.der ;
+    //                     ultimo_nodo_buscado.padre.izq = ultimo_nodo_buscado;         
+    //                     cardinal --;  
+    //                 } else { // el elemento a eliminar es mayor que el padre 
+    //                     ultimo_nodo_buscado.padre.der = ultimo_nodo_buscado.der ;
+    //                     ultimo_nodo_buscado.der.padre = ultimo_nodo_buscado.padre;
+    //                     cardinal --;  // el elemento a eliminar es mayor que el padre                
+    //                 }
+    //             } else {  // el hijo del elemento, esta del lado izquierdo
+    //                 if (ultimo_nodo_buscado.valor.compareTo(ultimo_nodo_buscado.padre.valor)<0){ 
+    //                     ultimo_nodo_buscado = ultimo_nodo_buscado.izq ;
+    //                     ultimo_nodo_buscado.padre.izq = ultimo_nodo_buscado;         
+    //                     cardinal --;  // el elemento a eliminar es menor que el padre
+    //                 } else {
+    //                     ultimo_nodo_buscado.padre.der = ultimo_nodo_buscado.izq ;
+    //                     ultimo_nodo_buscado.izq.padre = ultimo_nodo_buscado.padre;
+    //                     cardinal --;   // el elemento a eliminar es mayor que el padre                  
+    //                 }  
            
-                }        
-            } else { // Tiene 2 hijos
-                Nodo sucesor = busqueda_minimo(ultimo_nodo_buscado.der);
-                if (sucesor.izq==null && sucesor.der==null){ // si el sucesor no tiene hijos
-                    ultimo_nodo_buscado = sucesor ;
-                    ultimo_nodo_buscado.padre.der = ultimo_nodo_buscado ;
-                    ultimo_nodo_buscado.izq.padre = ultimo_nodo_buscado.padre.der;
-                    cardinal --;
+    //             }        
+    //         } else { // Tiene 2 hijos
+    //                 Nodo sucesor = busqueda_minimo(ultimo_nodo_buscado.der);     
+    //                 ultimo_nodo_buscado.valor = sucesor.valor ;
+    //                 // eliminar(sucesor.valor);
+    //                 cardinal --;
+    //             }
+    //         } 
+    // }
+
+    public void eliminar(T elem) {
+        Nodo nodo = buscar_nodo(elem, raiz);
+
+        // Si el ABB es vacio o el nodo no existe, no hacemos nada
+        if (nodo == null || nodo.valor.compareTo(elem) != 0) {
+            return;
+        }
+
+        // Caso 1: Nodo con dos hijos
+        if (nodo.izq != null && nodo.der != null) {
+            Nodo sucesor = busqueda_minimo(nodo.der);
+            T valorSucesor = sucesor.valor;
+            eliminar(valorSucesor); // eliminamos el nodo sucesor (que tendrá 0 o 1 hijo)
+            nodo.valor = valorSucesor; // reemplazamos el valor
+        }
+
+        // Caso 2 y 3: Nodo con 0 o 1 hijo
+        else {
+            Nodo hijo = (nodo.izq != null) ? nodo.izq : nodo.der; // puede ser null si es hoja
+
+            if (nodo == raiz) {
+                raiz = hijo;
+                if (hijo != null) hijo.padre = null;
+            } else {
+                Nodo padre = nodo.padre;
+                if (padre.izq == nodo) {
+                    padre.izq = hijo;
+                } else {
+                    padre.der = hijo;
                 }
+                if (hijo != null) hijo.padre = padre;
             }
+
+            cardinal--;
         }
     }
 
